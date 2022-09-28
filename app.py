631ce849a18206772a0d1cf507f6388a61a536d0
Michael <r121221@yandex.ru>
2022-09-28 19:45:26 +0300

    добавил html

--- app.py
+++ app.py
@@ -10,6 +10,16 @@ def hello():
     return render_template('index.html', user=user)
 
 
+@app.route('/<int:id>')
+def users(id):
+    return f'<h1>Ваш порядковый номер {id} </h1>'
+
+
+@app.route('/<name>')
+def people(name):
+    return render_template('people.html', name=name)
+
+
 @app.route('/help/')
 def help():
     return '<h1>Мы уже выехали </h1>'
@@ -17,30 +27,7 @@ def help():
 
 @app.route('/table/')
 def t1():
-    return '''
-<table  border="1" width="100%" cellpadding="5">  
-<caption>Перечень продуктов</caption>
-  <tr>
-    <th>№ п/п</th>
-    <th>Наименование товара</th>
-    <th>Ед. изм.</th>
-    <th>Количество</th>
-    <th>Цена за ед. изм., руб.</th>
-    <th>Стоимость, руб.</th>
-  </tr>
-  <tr>
-    <td>1.</td>
-    <td>Томаты свежие</td><td>кг</td><td>15,20</td><td>69,00</td><td>1048,80</td>
-  </tr>
-  <tr>
-    <td>2.</td>
-    <td>Огурцы свежие</td><td>кг</td><td>2,50</td><td>48,00</td><td>120,00</td>
-  </tr>
-  <tr>
-    <td colspan="5" style="text-align:right">ИТОГО:</td><td>1168,80</td>
-  </tr>
-</table>
-'''
+    return render_template('table.html')
 
 
 if __name__ == '__main__':
