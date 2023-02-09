19b1c9c62a304cc893d8ccbf177b1c3f86ffdfb7
Michael <r121221@yandex.ru>
2023-02-09 20:01:08 +0300

    добавил картинки

--- fask_db_class.py
+++ fask_db_class.py
@@ -72,11 +72,12 @@ class FlaskDataBase:
             sql = ''' SELECT title, text FROM posts WHERE url LIKE ? LIMIT 1'''
             self.__cur.execute(sql, (alias,))
             res = self.__cur.fetchone()
+            print(res['text'])
             if res:
                 base = url_for('static', filename='images_html')
                 text = re.sub(r"(?P<tag><img\s+[^>]*src=)(?P<quote>[\"'])(?P<url>.+?)(?P=quote)>",
                               "\\g<tag>" + base + "/\\g<url>>", res['text'])
-                print(text)
+
                 return (res['title'], text)
         except sqlite3.Error as e:
             print('Ошибка получения статьи из  БД', str(e))
